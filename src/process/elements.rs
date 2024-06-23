8faf1a3b026cef6c253735c87b37db08b2a5bc6d
Frank Jimenez <jjcfrank@gmail.com>
2024-06-23 19:54:51 +0100

    add comments to creation of nodes

--- src/process/elements.rs
+++ src/process/elements.rs
@@ -22,6 +22,12 @@ pub fn process_nodes<'a>(
     nodes_reader.for_each(|element| match element {
         Element::Node(n) => {
             let id = n.id() as i32;
+            // Here logic to choose between omt or custom
+            // 'omt' should be a flag in the CLI. If 'omt' is true,
+            // then we use the OMT logic, else we use the custom logic.
+            // i.e.
+            // let configs = try_configs(&args.schema_yamls)?;
+            //
             let properties = sort_tags(OsmType::Node(&n), &configs);
             let geom = Geometry::Point(Point::new(n.lon(), n.lat()));
             let wkb = geom_to_wkb(&geom).expect("Failed to insert node into database");
@@ -31,6 +37,12 @@ pub fn process_nodes<'a>(
         }
         Element::DenseNode(d) => {
             let id = d.id() as i32;
+            // Here logic to choose between omt or custom
+            // 'omt' should be a flag in the CLI. If 'omt' is true,
+            // then we use the OMT logic, else we use the custom logic.
+            // i.e.
+            // let configs = try_configs(&args.schema_yamls)?;
+            //
             let properties = sort_tags(OsmType::DenseNode(&d), &configs);
             let geom = Geometry::Point(Point::new(d.lon(), d.lat()));
             let wkb = geom_to_wkb(&geom).expect("Failed to insert node into database");
--- src/process/properties.rs
+++ src/process/properties.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 
 use osmpbf::{DenseNode, Node, Relation, Way};
 
-use crate::utils::config::Config;
+use crate::utils::config::{Config, Kvat};
 use crate::utils::validate::{try_mapping, FieldType};
 
 pub struct Properties {
@@ -104,19 +104,31 @@ fn sort_class<'a>(
     config: &'a Config,
     tags: &'a HashMap<&'a str, &'a str>,
 ) -> Result<(String, String)> {
+    // Iterates over all yaml files
     for class in config.class.iter() {
+        // Iterates over all kvat (key, values, and, then)
+        // in the 'class' object
         for kvat in class.iter() {
+            // Get the key matching the kvat key
             if let Some(values) = tags.get(kvat.key.as_str()) {
+                // Check if the value is in the kvat values
                 if kvat.values.contains(&values.to_string()) {
+                    // Also checks if there is an 'and' object
                     if let Some(and) = &kvat.and {
+                        // If there is an 'and' objects associated to the kvat
+                        // then iterate over all kvs (key, values) within the 'and' object
                         for kvs in and.iter() {
+                            // Check if the and.key is in the tags
                             if let Some(value) = tags.get(kvs.key.as_str()) {
+                                // Check if the value is in the kvs values
                                 if kvs.values.contains(&value.to_string()) {
+                                    // If the value is in the kvs values
                                     return Ok(("class".into(), kvat.then.clone()));
                                 }
                             }
                         }
                     } else {
+                        // If there is no 'and' object associated to the kvat
                         return Ok(("class".into(), kvat.then.clone()));
                     }
                 }
@@ -125,7 +137,6 @@ fn sort_class<'a>(
     }
 
     Err("Failed to sort class".into())
-
 }
 
 #[cfg(test)]
@@ -138,5 +149,37 @@ mod tests {
         let mut properties = Properties::new();
         properties.add("key", FieldType::Text("value".to_string()));
         assert_eq!(properties.jsonb["key"], json!("value"));
+    }
+
+    // Test that sort_class sorts the class based on yaml
+    #[test]
+    fn test_sort_class() {
+        let mut tags: HashMap<&str, &str> = HashMap::new();
+        tags.insert("key", "value");
+
+        let kvat = Kvat {
+            key: "key".to_string(),
+            values: vec!["value".to_string()],
+            and: None,
+            then: "class".to_string(),
+        };
+
+        let class = vec![kvat];
+
+        let config = Config {
+            schema: "omt".to_string(),
+            layer: "test".to_string(),
+            geometry_types: vec![
+                "Point".to_string(),
+                "LineString".to_string(),
+                "Polygon".to_string(),
+            ],
+            fields: vec![],
+            class: Some(class),
+        };
+
+        let result = sort_class(&config, &tags).unwrap();
+        assert_eq!(result, ("class".to_string(), "class".to_string()));
+
     }
 }
